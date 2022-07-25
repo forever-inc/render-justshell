1a533fe11240f0e8f524a2078500d1effe15e02f
alex simeone <asimeone@forever.com>
2022-07-25 10:37:19 -0400

    adding some echo

--- db_transfer.sh
+++ db_transfer.sh
@@ -7,22 +7,27 @@ start=`date +%s`
 pg_dump $STORE_DUMP_URL -f db.dump -F c
 pg_restore -c -O -F c -d $STORE_DB_URL db.dump
 rm db.dump
+echo "store complete"
 
 pg_dump $MARS_DUMP_URL -f db.dump -F c
 pg_restore -c -O -F c -d $MARS_DB_URL db.dump
 rm db.dump
+echo "mars complete"
 
 pg_dump $NOTIFICATIONS_DUMP_URL -f db.dump -F c
 pg_restore -c -O -F c -d $NOTIFICATIONS_DB_URL db.dump
 rm db.dump
+echo "notifications complete"
 
 pg_dump $DESKTOP_API_DUMP_URL -f db.dump -F c
 pg_restore -c -O -F c -d $DESKTOP_API_DB_URL db.dump
 rm db.dump
+echo "desktop-api complete"
 
 pg_dump $CONVERSION_DUMP_URL -f db.dump -F c
 pg_restore -c -O -F c -d $CONVERSION_DB_URL db.dump
 rm db.dump
+echo "conversion complete"
 
 end=`date +%s`
 
