a0b975124fa3f48f12650e94f83e75ecf4298ddd
alex simeone <asimeone@forever.com>
2022-12-28 15:07:44 -0500

    adding script to restore staging for pg14

---
+++ db_transfer_sandbox.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+cd /var/data
+
+start=`date +%s`
+
+pg_dump $STORE_DB_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $STORE_DB_URL_RENDER_14 db.dump
+rm db.dump
+echo "store complete"
+
+pg_dump $NOTIFICATIONS_DB_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $NOTIFICATIONS_DB_URL_RENDER_14 db.dump
+rm db.dump
+echo "notifications complete"
+
+pg_dump $DESKTOP_API_DB_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $DESKTOP_API_DB_URL_RENDER_14 db.dump
+rm db.dump
+echo "desktop-api complete"
+
+pg_dump $CONVERSION_DB_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $CONVERSION_DB_URL_RENDER_14 db.dump
+rm db.dump
+echo "conversion complete"
+
+pg_dump $MARS_MAIN_DB_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $MARS_MAIN_DB_URL_RENDER_14 db.dump
+rm db.dump
+echo "mars complete"
+
+pg_dump $SHARD_1_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $SHARD_1_URL_RENDER_14 db.dump
+rm db.dump
+echo "shard 1 complete"
+
+pg_dump $SHARD_2_URL_RENDER -f db.dump -F c
+pg_restore -c -O -F c -d $SHARD_2_URL_RENDER_14 db.dump
+rm db.dump
+echo "shard 2 complete"
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo "it took $runtime to execute this script."
+
